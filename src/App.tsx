a17651340fa63cb1ceddc28bdd7191a511e1981d
aida-rodi <aida.rodi.1997@gmail.com>
2023-06-15 12:45:20 +0200

    exercise 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
 import './App.css';
+import { useState } from 'react';
+import { data } from './assets/data';
+
+const formattedPrice = (price: number) => `${price.toFixed(0)}€`;
 
 function App() {
+  const [checkedState, setCheckedState] = useState(
+    new Array(data.length).fill(false)
+  );
+
+  const [total, setTotal] = useState(0);
+
+  const changeState = (index: number) => {
+    const updatedCheckedState = checkedState.map((state, i) =>
+      i === index ? !state : state
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + data[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setTotal(totalPrice);
+  };
+
   return (
     <>
-      <h1>React - Sprint 7</h1>
+      <p>What would you like to include in your budget?</p>
+
+      {data.map(({ text }, index) => {
+        return (
+          <p key={index}>
+            <input
+              type="checkbox"
+              id={`checkbox-${index}`}
+              checked={checkedState[index]}
+              onChange={() => changeState(index)}
+            />
+            <label>{text}</label>
+          </p>
+        );
+      })}
+
+      <p>Total: {formattedPrice(total)}</p>
     </>
   );
 }
---
+++ src/assets/data.tsx
@@ -0,0 +1,14 @@
+export const data = [
+    {
+      text: "Website (500€)",
+      price: 500
+    },
+    {
+      text: "SEO consultancy (300€)",
+      price: 300
+    },
+    {
+      text: "Google Ads campaign (200€)",
+      price: 200
+    },
+  ];
